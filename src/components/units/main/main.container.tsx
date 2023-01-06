0fb3261deb5680ba0d895e8486e17adb77a6f24b
hyeok96 <saing0813@naver.com>
2023-01-06 11:16:35 +0900

    [Fix] fixed contract

--- src/components/units/main/main.container.tsx
+++ src/components/units/main/main.container.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 //
 import MainPresenter from "./main.presenter";
@@ -12,8 +12,9 @@ declare const window: typeof globalThis & {
 
 const MainContainer = () => {
   const [wallet, setWallet] = useState<string>("");
-  const [dice1, setDice1] = useState(1);
-  const [dice2, setDice2] = useState(1);
+  const [bettingPrice, setBettingPrice] = useState<number>(0);
+  const [dice1, setDice1] = useState<number>(1);
+  const [dice2, setDice2] = useState<number>(1);
 
   const DiceResultArray = [
     "주사위 합",
@@ -30,6 +31,14 @@ const MainContainer = () => {
     "12: x3",
   ];
 
+  const onChangeBettingPrice = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    setBettingPrice(Number((e.target as HTMLInputElement).value));
+    console.log(Number(e.target.value) * 10 ** 18);
+  };
+
+  console.log(bettingPrice, "bettingPrice");
+
   const onClickConnectWallet = async () => {
     if (window.ethereum) {
       try {
@@ -37,6 +46,7 @@ const MainContainer = () => {
           method: "eth_requestAccounts",
         });
         setWallet(account[0]);
+        alert("Connect Wallet!!");
       } catch (error) {}
     } else {
       alert("Meta Mask not detected! Install Meta Mask!");
@@ -46,10 +56,12 @@ const MainContainer = () => {
   const onClickRoll = async () => {
     try {
       const diceContract = await onClickContract();
-      const result = await diceContract.roll();
+      const result = await diceContract.roll(String(bettingPrice * 10 ** 18));
       if (result.wait.length === 1) {
         const dice1 = await diceContract.diceArr1();
         const dice2 = await diceContract.diceArr2();
+        console.log(parseInt(dice1[dice1.length - 1]._hex, 16), "aa");
+        console.log(parseInt(dice2[dice2.length - 1]._hex, 16), "bb");
         setDice1(parseInt(dice1[dice1.length - 1]._hex, 16));
         setDice2(parseInt(dice2[dice2.length - 1]._hex, 16));
       }
@@ -59,11 +71,15 @@ const MainContainer = () => {
     }
   };
 
+  console.log(dice1, "aaaa");
+  console.log(dice2, "bbbb");
+
   return (
     <MainPresenter
       dice1={dice1}
       dice2={dice2}
       DiceResultArray={DiceResultArray}
+      onChangeBettingPrice={onChangeBettingPrice}
       onClickConnectWallet={onClickConnectWallet}
       onClickRoll={onClickRoll}
     />
--- src/components/units/main/main.types.ts
+++ src/components/units/main/main.types.ts
@@ -1,7 +1,9 @@
+import { ChangeEvent } from "react";
 export interface IMainPresenterProps {
   dice1: number;
   dice2: number;
   DiceResultArray: Array<string>;
+  onChangeBettingPrice: (e: ChangeEvent<HTMLInputElement>) => void;
   onClickConnectWallet: () => void;
   onClickRoll: () => void;
 }
